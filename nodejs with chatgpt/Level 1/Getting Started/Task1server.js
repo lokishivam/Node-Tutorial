4087107f4f9249b4e8f28602e9477b5f532f897f
lokishivam <94531871+lokishivam@users.noreply.github.com>
2023-02-11 20:39:32 +0530

    beginners

---
+++ nodejs with chatgpt/Level 1/Getting Started/Task1server.js
@@ -0,0 +1,26 @@
+//Task 1 creating server
+
+const httpServer = require("http"); /*
+An HTTP (Hypertext Transfer Protocol) server is a type of web server that is designed to serve web pages over the Internet. 
+It is the component of the web infrastructure that is responsible for receiving HTTP requests from client devices
+ (such as browsers) and returning the requested web pages or other resources (such as images, videos, or APIs).
+*/
+
+httpServer.createServer((req, res) => {
+  res.writeHead(200, { "content-Type": "text/plain" });
+  res.end("Hello World");
+  /*This line writes the response body using the end method of the res object.
+   The response body is the message "Hello, World!", which will be displayed in the browser.*/
+}); /*
+This line sets the status code and response headers using the writeHead method of the res object. 
+The 200 status code means "OK," and the Content-Type header specifies the type of the response body, 
+which is plain text.
+*/
+
+httpServer.listen(3000, () => {
+  console.log("listning at port 3000");
+});
+/* This line starts the HTTP server and makes it listen for incoming requests on port 3000. 
+The listen method takes a port number and an optional callback function as arguments.
+ The callback function is executed when the server is ready to receive requests, 
+ and it simply logs a message to the console.*/
